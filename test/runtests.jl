ca6da03bcbdb390d8695bc75dd238cf102fb4b3b
Matti Pastell <matti.pastell@helsinki.fi>
2014-12-22 16:52:52 +0200

    Gadfly tests should now pass also with Julia 0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,13 +4,14 @@ using Base.Test
 include("chunk_options.jl")
 #include("pyplot_formats.jl") cause segfaults, but OK if run by itself
 
-#Test Gadfly and markdown reader, Gadfly only works with 0.3
-if VERSION.minor == 3
-    weave("documents/gadfly_markdown_test.jmd", doctype="github",plotlib="gadfly", informat="markdown")
-    result = readall(open("documents/gadfly_markdown_test.md"))
-    ref = readall(open("documents/gadfly_markdown_test_ref.md"))
-    @test result == ref
+weave("documents/gadfly_markdown_test.jmd", doctype="github",plotlib="gadfly", informat="markdown")
+result = readall(open("documents/gadfly_markdown_test.md"))
+ref = readall(open("documents/gadfly_markdown_test_ref.md"))
+@test result == ref
 
+#Test winston only for 0.3
+if VERSION.minor == 3
     include("winston_formats.jl")
-    include("gadfly_formats.jl")
 end
+
+include("gadfly_formats.jl")
