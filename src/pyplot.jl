62a9cff75359cef6dcfca8e89b8287165d672d36
wildart <wildart@gmail.com>
2015-10-28 16:34:10 -0400

    correct pyplot syntax

--- src/pyplot.jl
+++ src/pyplot.jl
@@ -7,12 +7,12 @@ function savefigs_pyplot(chunk, report::Report)
     isdir(figpath) || mkdir(figpath)
     chunkid = (chunk.options[:name] == nothing) ? chunk.number : chunk.options[:name]
     #Iterate over all open figures, save them and store names
-    for fig = plt[:get_fignums]
+    for fig in plt[:get_fignums]()
         full_name, rel_name = get_figname(report, chunk, fignum=fig)
         savefig(full_name, dpi=chunk.options[:dpi])
         push!(fignames, rel_name)
-        plt[:draw]
-        plt[:close]
+        plt[:draw]()
+        plt[:close]()
     end
     return fignames
 end
