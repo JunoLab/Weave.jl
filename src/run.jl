8324d32f0154bf529ea34bfa10ad072bf7d3dce0
Tony Kelman <tony@kelman.net>
2016-09-24 01:06:06 -0400

    No need to eval(parse(include(...)))
    just include

--- src/run.jl
+++ src/run.jl
@@ -279,16 +279,16 @@ function init_plotting(plotlib)
         l_plotlib = lowercase(plotlib)
         rcParams[:chunk_defaults][:fig] = true
         if l_plotlib == "winston"
-            eval(parse("""include(joinpath(dirname(@__FILE__),"winston.jl"))"""))
+            include("winston.jl")
             rcParams[:plotlib] = "Winston"
         elseif l_plotlib == "pyplot"
-            eval(parse("""include(joinpath(dirname(@__FILE__),"pyplot.jl"))"""))
+            include("pyplot.jl")
             rcParams[:plotlib] = "PyPlot"
         elseif l_plotlib == "plots"
-            eval(parse("""include(joinpath(dirname(@__FILE__),"plots.jl"))"""))
+            include("plots.jl")
             rcParams[:plotlib] = "Plots"
         elseif l_plotlib == "gadfly"
-            eval(parse("""include(joinpath(dirname(@__FILE__),"gadfly.jl"))"""))
+            include("gadfly.jl")
             rcParams[:plotlib] = "Gadfly"
       end
     end
