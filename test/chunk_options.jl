99fbfeca90c8b34b73fc5a0512e5cf35bdcee8e6
Matti Pastell <matti.pastell@helsinki.fi>
2016-11-03 14:11:08 +0200

    Remove compat from chunk_options.jl

--- test/chunk_options.jl
+++ test/chunk_options.jl
@@ -5,62 +5,62 @@ cleanup = true
 
 #Test chunk options and output formats
 weave("documents/chunk_options.noweb", plotlib=nothing)
-result = @compat readstring(open("documents/chunk_options.md"))
-ref = @compat readstring(open("documents/chunk_options_ref.md"))
+result =  readstring(open("documents/chunk_options.md"))
+ref =  readstring(open("documents/chunk_options_ref.md"))
 @test result == ref
 cleanup && rm("documents/chunk_options.md")
 
 weave("documents/chunk_options.noweb", doctype="tex", plotlib=nothing)
-result = @compat readstring(open("documents/chunk_options.tex"))
-ref = @compat readstring(open("documents/chunk_options_ref.tex"))
+result =  readstring(open("documents/chunk_options.tex"))
+ref =  readstring(open("documents/chunk_options_ref.tex"))
 @test result == ref
 cleanup && rm("documents/chunk_options.tex")
 
 weave("documents/chunk_options.noweb", doctype="texminted", plotlib=nothing)
-result = @compat readstring(open("documents/chunk_options.tex"))
-ref = @compat readstring(open("documents/chunk_options_ref.texminted"))
+result =  readstring(open("documents/chunk_options.tex"))
+ref =  readstring(open("documents/chunk_options_ref.texminted"))
 @test result == ref
 cleanup && rm("documents/chunk_options.tex")
 
 weave("documents/chunk_options.noweb", doctype="rst", plotlib=nothing)
-result = @compat readstring(open("documents/chunk_options.rst"))
-ref = @compat readstring(open("documents/chunk_options_ref.rst"))
+result =  readstring(open("documents/chunk_options.rst"))
+ref =  readstring(open("documents/chunk_options_ref.rst"))
 @test result == ref
 cleanup && rm("documents/chunk_options.rst")
 
 #Test out_path
 weave("documents/chunk_options.noweb", doctype="rst",
       out_path="documents/outpath_options.rst" , plotlib=nothing)
-result = @compat readstring(open("documents/outpath_options.rst"))
-ref = @compat readstring(open("documents/chunk_options_ref.rst"))
+result =  readstring(open("documents/outpath_options.rst"))
+ref =  readstring(open("documents/chunk_options_ref.rst"))
 @test result == ref
 cleanup && rm("documents/outpath_options.rst")
 
 #Test tangle
 tangle("documents/chunk_options.noweb")
-result = @compat readstring(open("documents/chunk_options.jl"))
-ref = @compat readstring(open("documents/chunk_options_ref.jl"))
+result =  readstring(open("documents/chunk_options.jl"))
+ref =  readstring(open("documents/chunk_options_ref.jl"))
 @test result == ref
 cleanup && rm("documents/chunk_options.jl")
 
 
 tangle("documents/chunk_options.noweb", out_path = "documents/outoptions.jl")
-result = @compat readstring(open("documents/outoptions.jl"))
-ref = @compat readstring(open("documents/chunk_options_ref.jl"))
+result =  readstring(open("documents/outoptions.jl"))
+ref =  readstring(open("documents/chunk_options_ref.jl"))
 @test result == ref
 cleanup && rm("documents/outoptions.jl")
 
 
 #Test functions and sandbox clearing
 weave("documents/chunk_func.noweb", plotlib=nothing)
-result = @compat readstring(open("documents/chunk_func.md"))
-ref = @compat readstring(open("documents/chunk_func_ref.md"))
+result =  readstring(open("documents/chunk_func.md"))
+ref =  readstring(open("documents/chunk_func_ref.md"))
 @test result == ref
 cleanup && rm("documents/chunk_func.md")
 
 #Test term=true
 weave("documents/test_term.jmd", doctype = "pandoc")
-result = @compat readstring(open("documents/test_term.md"))
-ref = @compat readstring(open("documents/test_term_ref.md"))
+result =  readstring(open("documents/test_term.md"))
+ref =  readstring(open("documents/test_term_ref.md"))
 @test result == ref
 cleanup && rm("documents/test_term.md")
