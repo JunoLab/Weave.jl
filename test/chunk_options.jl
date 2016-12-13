6de5ba342ce95a357b3de13dc57751d946559c06
Matti Pastell <matti.pastell@helsinki.fi>
2016-12-13 22:18:54 +0200

    Added test for tangle

--- test/chunk_options.jl
+++ test/chunk_options.jl
@@ -8,3 +8,10 @@ result =  readstring(open("documents/chunk_options.md"))
 ref =  readstring(open("documents/chunk_options_ref.md"))
 @test result == ref
 cleanup && rm("documents/chunk_options.md")
+
+
+tangle("documents/chunk_options.noweb", out_path = "documents/tangle")
+result =  readstring(open("documents/tangle/chunk_options.jl"))
+ref =  readstring(open("documents/tangle/chunk_options.jl.ref"))
+@test ref == result
+cleanup && rm("documents/tangle/chunk_options.jl")
---
+++ test/documents/tangle/chunk_options.jl.ref
@@ -0,0 +1,23 @@
+
+y= [2, 5, 12]
+
+x = [12, 10]
+println(y)
+println(x)
+
+println("Results without code")
+println(x)
+
+y = 1:5
+println(y)
+
+a = "Don't print me"
+println(a)
+
+println("No markup for results.")
+
+println(collect(0:10:1000))
+
+println(collect(0:10:1000))
+
+println(collect(0:10:1000))
