c62fb709d182633088a9c6868bde7ec1fe50d9e1
Matti Pastell <matti.pastell@helsinki.fi>
2016-12-19 13:13:47 +0200

    Added test for notebook reader

--- test/convert_test.jl
+++ test/convert_test.jl
@@ -15,3 +15,15 @@ convert_test("chunk_options.jmd")
 convert_test("chunk_options.jl")
 convert_test("chunk_options.mdw")
 convert_test("chunk_options.ipynb")
+
+function convert_test_nb(outfile)
+  outfile = joinpath("documents/convert", outfile)
+  infile = "documents/chunk_options.ipynb"
+  convert_doc(infile, outfile)
+  result =  readstring(open(outfile))
+  ref =  readstring(open(outfile * ".ref"))
+  rm(outfile)
+  @test result == ref
+end
+
+convert_test_nb("chunk_options_nb.mdw")
