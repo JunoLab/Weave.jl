65af83115259c524f40df28ce85fa056e872d0a4
Matti Pastell <matti.pastell@helsinki.fi>
2018-01-03 11:59:43 +0200

    New mimetype tests for output

--- test/rich_output.jl
+++ test/rich_output.jl
@@ -2,13 +2,15 @@ using Weave
 using Base.Test
 
 function mmtest(source, resfile, doctype)
-  VER = "$(VERSION.major).$(VERSION.minor)"
-  weave("documents/$source", out_path = "documents/multimedia/$resfile", doctype=doctype, plotlib=nothing)
+  weave("documents/$source", out_path = "documents/multimedia/$resfile", 
+    doctype=doctype, plotlib=nothing, template = "templates/mini.tpl")
   result =  readstring("documents/multimedia/$resfile")
-  ref =  readstring("documents/multimedia/$VER/$resfile.ref")
+  ref =  readstring("documents/multimedia/$resfile.ref")
   @test result == ref
   rm("documents/multimedia/$resfile")
 end
 
 mmtest("rich_output.jmd", "rich_output.md", "pandoc")
 mmtest("rich_output.jmd", "rich_output.tex", "tex")
+mmtest("rich_output.jmd", "rich_output.html", "md2html")
+mmtest("rich_output.jmd", "rich_output.github", "github")
