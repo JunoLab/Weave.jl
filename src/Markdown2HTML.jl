8ab0d66dda4920c760de2b168f661de309b73c15
Matti Pastell <matti.pastell@helsinki.fi>
2018-01-07 21:33:50 +0200

    Fix html list writer

--- src/Markdown2HTML.jl
+++ src/Markdown2HTML.jl
@@ -2,8 +2,7 @@ module Markdown2HTML
 # Markdown to HTML writer, Modified from Julia Base.Markdown html writer
 using Base.Markdown: MD, Header, Code, Paragraph, BlockQuote, Footnote,
       Admonition, List, HorizontalRule, Bold, Italic, Image, Link, LineBreak,
-      LaTeX
-
+      LaTeX, isordered
 
 function tohtml(io::IO, m::MIME"text/html", x)
     show(io, m, x)
