4bf8e0e209402ccb4a4500229db48d6d06654160
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-02-01 10:59:39 -0500

    check fig_ext to choose preferred mimetype in display(::Report, d)

--- src/display_methods.jl
+++ src/display_methods.jl
@@ -31,9 +31,17 @@ const default_mime_types = ["image/svg+xml", "image/png", "text/html", "text/pla
 #From IJulia as a reminder
 #const supported_mime_types = [ "text/html", "text/latex", "image/svg+xml", "image/png", "image/jpeg", "text/plain", "text/markdown" ]
 
+const mimetype_from_fig_ext =
+    Dict(".png" => "image/png",
+         ".jpg" => "image/jpeg",
+         ".jpeg" => "image/jpeg",
+         ".svg" => "image/svg+xml",
+         ".pdf" => "application/pdf")
+
 function Base.display(report::Report, data)
     #Set preferred mimetypes for report based on format
-    for m in report.mimetypes
+    fig_ext = report.cur_chunk.options[:fig_ext]
+    for m in Iterators.flatten(((mimetype_from_fig_ext[fig_ext], ), report.mimetypes))
         if showable(m, data)
             try
                 if !istextmime(m)
