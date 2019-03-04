6510ea0556809fa5c523ff79e8d28f074963246d
Matti Pastell <matti.pastell@helsinki.fi>
2019-03-04 15:10:03 +0200

    Documenter build (#191)
    * Add documenter to travis

--- doc/make.jl
+++ doc/make.jl
@@ -1,8 +1,13 @@
 using Documenter, Weave
-
+cd("doc")
 makedocs( modules = Weave, sitename="Weave.jl",
     pages = ["index.md", "getting_started.md", "usage.md",
     "publish.md", "chunk_options.md", "notebooks.md",
     "function_index.md"]
 )
 include("make_examples.jl")
+
+deploydocs(
+    repo = "github.com/mpastell/Weave.jl.git",
+    target = "build"
+)
