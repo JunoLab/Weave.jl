c34d8d909f1f9df6fcaa1edfbda14de89a312bb7
Arnav Sood <soodarnav01@gmail.com>
2019-03-12 12:29:48 -0700

    Conda for CI

--- test/notebooks.jl
+++ test/notebooks.jl
@@ -1,5 +1,8 @@
 file = joinpath(@__DIR__, "documents", "jupyter_test.jmd")
-using IJulia
+using IJulia, Conda
 
-Weave.notebook(file)
+Conda.add("jupyter")
+Conda.add("nbconvert")
+
+Weave.notebook(file, jupyter_path = joinpath(Conda.ROOTENV, "bin", "jupyter"))
 @test "temp_notebook.ipynb" ∈ readdir(@__DIR__) # test if the result was weaved
