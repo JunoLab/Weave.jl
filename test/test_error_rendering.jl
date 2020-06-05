c7b566aaaaa9c1ccb5496c93a126f240dbffeaac
Shuhei Kadowaki <aviatesk@gmail.com>
2020-06-05 18:59:02 +0900

    fix header regression, add test, update test

--- test/test_error_rendering.jl
+++ test/test_error_rendering.jl
@@ -35,7 +35,7 @@ err_str3_1 = get_err_str("plot(x)")
 err_str3_2 = get_err_str("f(y")
 
 
-let doc = mock_doc(str; doctype = "github")
+let doc = mock_run(str; doctype = "github")
     get_output(i) = doc.chunks[i].output
 
     @test occursin(err_str1, get_output(1))
@@ -44,6 +44,6 @@ let doc = mock_doc(str; doctype = "github")
     @test occursin(err_str3_2, get_output(3))
 end
 
-@test_throws ArgumentError mock_doc(str; doctype = "github", throw_errors = true)
+@test_throws ArgumentError mock_run(str; doctype = "github", throw_errors = true)
 
 # TODO: test error rendering in `rich_output`
